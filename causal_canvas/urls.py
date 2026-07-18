eae636abc4111e2cdac9e7c26531415f7fb6d38a
Ivan Ivanov <senekazx@gmail.com>
2026-07-19 00:49:19 +0300

    Add static serving via Django

--- causal_canvas/urls.py
+++ causal_canvas/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('simulator.urls')),
-    path('', include('simulator.urls')),  # для отображения шаблона, но мы сделаем отдельно
+    path('', include('simulator.urls')),
 ]
+
+# Добавляем раздачу статических файлов через Django
+# (даже если DEBUG=False, это будет работать, пока STATIC_ROOT существует)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
