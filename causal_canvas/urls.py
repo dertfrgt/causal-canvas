3164a6ecaa6d5336e43380f3efb09e9672753263
Ivan Ivanov <senekazx@gmail.com>
2026-07-19 12:21:47 +0300

    Remove manual static serving, keep WhiteNoise

--- causal_canvas/urls.py
+++ causal_canvas/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('simulator.urls')),
-]
-
-# Раздача статики (работает даже при DEBUG=False)
-urlpatterns += [
-    path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
 ]
